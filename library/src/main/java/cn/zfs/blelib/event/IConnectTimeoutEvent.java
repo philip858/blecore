5a79059b71e07b50e500b289fd01512736cae75b
fszeng2011 <zeng_fs@yeah.net>
2018-09-09 12:15:04 +0800

    拆分连接事件接口

---
+++ library/src/main/java/cn/zfs/blelib/event/IConnectTimeoutEvent.java
@@ -0,0 +1,15 @@
+package cn.zfs.blelib.event;
+
+/**
+ * 描述: 蓝牙连接事件
+ * 使用方法: 在要监听的类中实现接口，并在方法上添加上@Subscribe注解
+ * 时间: 2018/9/8 11:49
+ * 作者: zengfansheng
+ */
+public interface IConnectTimeoutEvent {
+
+    /**
+     * 连接超时
+     */
+    void onConnectTimeout(Events.ConnectTimeout event);
+}
